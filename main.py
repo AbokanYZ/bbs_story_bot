632c8640c5a6d2e0dd1c17c52f8cdf7ff8a2274a
AbokanYZ <abokanyz@gmail.com>
2024-01-18 10:34:55 +0100

    Removed secondaryQuest() & 'continue' keyword from conditions

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pyautogui as pag
 import PIL
 import time
-import keyboard
 import sys
+
+path = {
+    "auto": "assets\\auto.png",
+    "skip": "assets\\skip.jpg",
+    "friend_request": "assets\\friend_request.jpg",
+    "touch_screen": "assets\\touch_screen.jpg",
+    "next_quest": "assets\\next_quest.png",
+    "prep_quest": "assets\\prep_quest.jpg",
+    "start_quest": "assets\\start_quest.jpg",
+    "gift": "assets\\close_gift.jpg",
+    "limited": "assets\\limited.jpg",
+    "hard": "assets\\hard.jpg",
+}
 
 def findAndClick(path):
     # Will find the image, then click it. 
@@ -30,28 +42,27 @@ def storyMode():
                 if findAndClick("assets\\auto.png") and auto == 0:
                     auto += 1
                     print('The bot is running...')
-                    time.sleep(60)
+                    time.sleep(60) # Until the quest is over
                     print('The bot completed the quest')
                     continue
             
                 elif findAndClick('assets\\skip.jpg'):
                     print('Conversation skipped')
-                    continue
-
-                elif findAndClick('assets\\skip.jpg'):
                     time.sleep(2)
-                    continue 
+                    pass
 
                 elif findAndClick('assets\\friend_request.jpg'):
-                    continue
+                    pass
 
                 elif findAndClick('assets\\touch_screen.jpg'):
-                    continue
+                    pass
 
                 elif findAndClick("assets\\next_quest.png"):
                     inMission = False
                     time.sleep(5)
                     continue
+
+                print(" ")
 
             else: # What to do when we're not in a mission
 
@@ -60,19 +71,19 @@ def storyMode():
 
                 elif findAndClick('assets\\prep_quest.jpg'):
                     time.sleep(5)
-                    continue
+
                 elif findAndClick('assets\\start_quest.jpg'):
                     inMission = True
                     time.sleep(5)
-                    continue
+
                 else:
                     print('The quest failed to start')
                     startFailed += 1
         else:
             return
 
-# def secondaryQuest(): 
-
+def secondaryQuest(): 
+    pass
 
 if len(sys.argv) != 2:
     print("Usage: python main.py <mode> ('story', 'secondary')")
@@ -83,6 +94,6 @@ mode = sys.argv[1].lower()
 if mode == "story":
     storyMode()
 elif mode == "secondary":
-    pass
+    secondaryQuest()
 else:
     print('No mode selected')
